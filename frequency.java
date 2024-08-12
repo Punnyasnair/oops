59026f457eb1e2702e569285ac9b85982044f1cb
Punnyasnair <punnyasnair2005@gmail.com>
2024-08-12 10:38:30 +0530

    Add files via upload

---
+++ frequency.java
@@ -0,0 +1,31 @@
+//program to find the frequency of character in a string
+//PUNNYA S NAIR
+//Roll no:50
+import java.util.*;
+class frequency
+{
+	public static void main(String args[])
+		{  
+		   int count=0;
+			Scanner S=new Scanner(System.in);
+			System.out.println("Enter String");
+			String s1;
+			s1=S.nextLine();
+			s1.toLowerCase();
+			System.out.println("Enter the character to check");
+		   char c= S.next().charAt(0);
+		   int n=s1.length();
+		   for(int i=0;i<n;i++)
+		   {
+		   	if(s1.charAt(i)== c)
+		   	{
+		   	 
+		   	
+		   		count++;
+		   	
+		   	}
+		   }
+		   System.out.println("The frequency is"+count);
+		   S.close();
+		   }
+}
